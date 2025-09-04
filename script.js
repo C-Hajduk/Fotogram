2f0ff19ba83a6f31e5ac4ed53f2cdba3d0947249
C.Hajduk <christian.hajduk@outlook.de>
2025-09-04 15:12:37 +0200

    structure HTMl & change Js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let imgArray = [
+const imgArray = [
   "assets/img/20230331_102806.jpg",
   "assets/img/20230331_102941.jpg",
   "assets/img/20230331_154828.jpg",
@@ -16,63 +16,47 @@ let imgArray = [
   "assets/img/20230405_152108.jpg",
 ];
 
-let bigImgArray = [
-  "assets/img/20230331_102806.jpg",
-  "assets/img/20230331_102941.jpg",
-  "assets/img/20230331_154828.jpg",
-  "assets/img/20230402_102032.jpg",
-  "assets/img/20230402_155314.jpg",
-  "assets/img/20230403_152724.jpg",
-  "assets/img/20230403_154436.jpg",
-  "assets/img/20230403_204057.jpg",
-  "assets/img/20230404_103648.jpg",
-  "assets/img/20230404_125938.jpg",
-  "assets/img/20230404_155440.jpg",
-  "assets/img/20230404_161925.jpg",
-  "assets/img/20230405_105337.jpg",
-  "assets/img/20230405_150408.jpg",
-  "assets/img/20230405_152108.jpg",
-];
+// ======================= img bilder in HTMl erstellen ===============================
 
-function loadImages() {
-  let images;
+// ======================= render Funktion ==================================
 
-  if (localStorage.getItem("images")) {
-    images = JSON.parse(localStorage.getItem("images"));
-  } else {
-    localStorage.setItem("images", JSON.stringify(imgArray));
-    images = imgArray;
-  }
+function renderImg() {
+  let imageGalleryRef = document.getElementById("imageGallery");
+  imageGalleryRef.innerHTML = "";
 
-  render(images);
-
-  function render(images) {
-    let dropImg = document.getElementById("dropImg");
-    dropImg.innerHTML = "";
-
-    if (images && images.length > 0) {
-      for (let index = 0; index < images.length; index++) {
-        let img = document.createElement("img");
-        img.src = images[index];
-        img.alt = "japanBild" + (index + 1);
-        img.loading = "charge";
-        img.style.width = "300px";
-        img.style.margin = "5px";
-        dropImg.appendChild(img);
-      }
-    }
+  for (let index = 0; index < imgArray.length; index++) {
+    imageGalleryRef.innerHTML += `<img class="thumbnailGallery" src="${imgArray[index]}" alt="">`;
   }
 }
 
-// Overlay für das Popup
-function toggleOverlay() {
-  let overlayRef = document.getElementById("bigImgOverlay");
+// ==================== Overlay für das Popup ==================================
 
-  overlayRef.classList.toggle("d_none");
+function renderDialog() {
+  // fuers rendern des Dialog
 }
 
-// Großes Bild im Overlay anzeigen
+// per click auf das Bild, öffnet sich ein dialog
+function openDialog() {
+  // wenn ich auf das img klicke, dann öffnet sich der Dialog
+  // ansonsten kann ich den Dialog nicht oeffnen
+}
 
 // Klick auf OverlayButton = schließen
+function closeDialog() {
+  // wenn ich den Button klicke, dann schliesst sich der Dialog
+}
 
 // Klick außerhalb des Bildes -> Overlay schließen
+function bubbling() {}
+
+// pfeiltaste zum swipen
+
+function forwardImg() {
+  // wenn ich den button klicke, dann swipe ich nach links
+}
+
+function backImg() {
+  // wenn ich den button klicke, dann swipe ich nach rechts
+}
+
+// ============================================================================
